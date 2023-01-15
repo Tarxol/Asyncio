4d3c95ef5f696159344cf93ea85cc842d1adc876
unknown <tarxol@yandex.ru>
2023-01-15 17:58:29 +0300

    third commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import asyncpg
 import aiohttp #асинхронный аналог requests
 import time
+import requests
 from more_itertools import chunked #генератор
 import config
 from typing import Iterable
@@ -24,10 +25,34 @@ async def get_person_in_db(session: aiohttp.client.ClientSession, person_id: int
         query = 'INSERT INTO heros (id, name, birth_year, eye_color, films, gender, hair_color, height, homeworld, ' \
                 'mass, skin_color, species, starships, vehicles) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,' \
                 ' $11, $12, $13, $14)'
-        value_films = ','.join(response_json.get('films')) if response_json.get('films') else ''
-        value_species = ','.join(response_json.get('species')) if response_json.get('species') else ''
-        value_starships = ','.join(response_json.get('starships')) if response_json.get('starships') else ''
-        value_vehicles = ','.join(response_json.get('vehicles')) if response_json.get('vehicles') else ''
+        # value_films = ','.join(response_json.get('films')) if response_json.get('films') else ''
+        films = []
+        for film in response_json.get('films'):
+            response_json_film = requests.get(film)
+            response_json_film_title = response_json_film.json()
+            films.append(response_json_film_title.get('title'))
+        value_films = ','.join(films)
+        # value_species = ','.join(response_json.get('species')) if response_json.get('species') else ''
+        species = []
+        for specie in response_json.get('species'):
+            response_json_specie = requests.get(specie)
+            response_json_specie_name = response_json_specie.json()
+            species.append(response_json_specie_name.get('name'))
+        value_species = ','.join(species)
+        # value_starships = ','.join(response_json.get('starships')) if response_json.get('starships') else ''
+        starships = []
+        for starship in response_json.get('starships'):
+            response_json_starship = requests.get(starship)
+            response_json_starship_name = response_json_starship.json()
+            starships.append(response_json_starship_name.get('name'))
+        value_starships = ','.join(starships)
+        # value_vehicles = ','.join(response_json.get('vehicles')) if response_json.get('vehicles') else ''
+        vehicles = []
+        for vehicle in response_json.get('vehicles'):
+            response_json_vehicle = requests.get(vehicle)
+            response_json_vehicle_name = response_json_vehicle.json()
+            vehicles.append(response_json_vehicle_name.get('name'))
+        value_vehicles = ','.join(vehicles)
         values_tuple = (person_id,
                         response_json.get('name', ''),
                         response_json.get('birth_year', ''),
